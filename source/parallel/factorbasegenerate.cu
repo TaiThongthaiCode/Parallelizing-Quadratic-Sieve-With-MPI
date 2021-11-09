de38136f5e265d8c4d38d1c32d2981950cb59f63
Nippana Thongthai <nthongt1@swarthmore.edu>
2021-11-08 21:25:22 -0500

    new files

---
+++ source/parallel/factorbasegenerate.cu
@@ -0,0 +1,12 @@
+/*
+Tai Thongthai and Tarang Saluja
+
+In the words of the illustrious travis scott: "bruhdda"
+*/
+
+
+#include "firesimulator.h"
+#include <handle_cuda_error.h>
+#include <curand_kernel.h>
+#include <curand.h>
+#include <sys/types.h>
