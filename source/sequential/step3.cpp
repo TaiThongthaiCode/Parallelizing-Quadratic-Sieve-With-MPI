527d3ae7937a7b50cf2ae9b003609678c93a2f4c
Tarang Saluja <tsaluja1@cs.swarthmore.edu>
2021-11-21 20:48:36 -0500

    Steps for part 3

--- source/sequential/step3.cpp
+++ source/sequential/step3.cpp
@@ -21,3 +21,13 @@ Tai Thongthai and Tarang Saluja
 
 
 #include "step3.h"
+
+//TODO
+
+//(1)  Insert the matrix row by row from the output of step2.cpp
+
+//(2) Use algorithm to get solutions over GF(2)
+
+//(3) For each solution (1) Find the number T_1, T_2 ... T_k and the square root of the product of primes (2) Subtract take two numbers and take gcd with N
+
+//Once solution is found, quit!
