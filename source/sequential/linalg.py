d84812ff32eaa69940ecf9e29e00b52d1ca0937e
Nippana Thongthai <nthongt1@swarthmore.edu>
2021-11-29 21:35:00 -0500

    Wrong Places by HER is an underrated song

---
+++ source/sequential/linalg.py
@@ -0,0 +1,20 @@
+"""
+"Candice" - Abraham Lincoln
+"""
+
+import qs_helper.py as qs
+
+def main():
+
+    N = 69
+    #need processing
+    bit_matrix = []
+    smooth_nums = []
+
+    a, b = qs.lin_alg_solve(bit_matrix, smooth_nums, N)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
