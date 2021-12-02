7ad04036da2fc7097396e89fde1d84a87c53fafb
Tarang Saluja <tsaluja1@cs.swarthmore.edu>
2021-12-02 15:36:34 -0500

    Lin Alg

--- source/sequential/linalg.py
+++ source/sequential/linalg.py
@@ -2,16 +2,47 @@
 "Candice" - Abraham Lincoln
 """
 
-import qs_helper.py as qs
+import qs_helper as qs
 
 def main():
-
-    N = 69
-    #need processing
+    N = 39203
     bit_matrix = []
     smooth_nums = []
 
+    with open("Expo_Matrix.txt") as f:
+        for line in f:
+            relation = [int(line[i])%2 for i in range(len(line)-1)]
+            bit_matrix.append(relation)
+        f.close()
+
+    with open("Smooth_Num.txt") as f:
+        for line in f:
+            smooth = int(line)
+            smooth_nums.append(smooth)
+
+        f.close()
+
+    # with open("factorbase.txt") as f:
+    #     for line in f:
+    #         factor_base.append(int(line.split()[0]))
+
+    #     f.close()
+
+    # bit_matrix = qs.build_matrix(smooth_nums, factor_base)
+    print("SIZE BIT", len(bit_matrix))
+    print("SIZE SMOOTH", len(smooth_nums))
+
+
+
+    bit_matrix.reverse()
+    smooth_nums.reverse()
+    # print(smooth_nums)
+
+
     a, b = qs.lin_alg_solve(bit_matrix, smooth_nums, N)
+
+    print(a)
+    print(b)
 
     return
 
