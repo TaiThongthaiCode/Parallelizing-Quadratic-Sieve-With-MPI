de5de3e8419f1785576cd506e6892d9dead5ac7b
Tarang Saluja <tsaluja1@cs.swarthmore.edu>
2021-12-11 18:28:25 -0500

    sequential ready to test

--- source/sequential/step2.cpp
+++ source/sequential/step2.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]){
 
     //current size of sieving interval
     //int pes = 386*size*size -23209.3*size + 2352768;
-    int pes = 800000;
+    int pes = 32000;
     int fbs;
     int relation_count = 0;
 
@@ -63,10 +63,10 @@ int main(int argc, char *argv[]){
 }
 
 /*
-Description: load in the factor base elements and the corresponding values 
-            of a and b which correspond to the smallest values a and b such 
+Description: load in the factor base elements and the corresponding values
+            of a and b which correspond to the smallest values a and b such
             that a^2 - T is 0 mod p and b^2 - T is 0 mod p.
-Params: (1) mpz_t number N (product of two primes); 
+Params: (1) mpz_t number N (product of two primes);
         (2) integer factor base size (fbs)
 Return: A pointer to prime_element array. The struct prime_element holds ints,
         prime p, and their solutions a and b.
@@ -114,11 +114,11 @@ prime_element * load(mpz_t N, int fbs){
 }
 
 /*
-Description: Generates our sieving interval given a size and starting bound. 
-Params: (1) mpz_t number N (product of two primes); 
+Description: Generates our sieving interval given a size and starting bound.
+Params: (1) mpz_t number N (product of two primes);
         (2) int size_SSI (interval size);
-        (3) mpz_t number T (interval starting bound starting bound). 
-Return: A pointer to the polynomial_element array. The polynomial_element 
+        (3) mpz_t number T (interval starting bound starting bound).
+Return: A pointer to the polynomial_element array. The polynomial_element
         struct holds an mpz_t type (we named "poly"). This is done
         due to weird memory constraints of mpz_t types
 */
@@ -149,17 +149,17 @@ polynomial_element * generate_sieving_interval(mpz_t N, int size_SSI, mpz_t T){
 
 
 /*
-Description: step where we repeatedly divide our sieving interval using our 
+Description: step where we repeatedly divide our sieving interval using our
               prime factor base until we have the required number of relations
               and writing the relevant data to file.
-Params: (1) Pointer to prime_element array (the factorbase); 
-        (2) mpz_t N; 
-        (3) int factorbasesize (fbs); 
+Params: (1) Pointer to prime_element array (the factorbase);
+        (2) mpz_t N;
+        (3) int factorbasesize (fbs);
         (4) int polynomial element array size(pes)
 Return: Nothing
-Output: [All Text Files]: (1) Power_Matrix.txt (matrix representation of
-                              prime powers of found relations); 
-                          (2) Bit_Matrix.txt (Power_Matrix mod 2); 
+Output [All Text Files]: (1) Power_Matrix.txt (matrix representation of
+                              prime powers of found relations);
+                          (2) Bit_Matrix.txt (Power_Matrix mod 2);
                           (3) Smooth_Numbers.txt (Numbers we have found to be smooth)
 */
 void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
@@ -173,7 +173,6 @@ void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
 
   unsigned long int init1, init2, step;
   string temp;
-
 
   int** power_storage;
   polynomial_element * SI;
@@ -203,9 +202,7 @@ void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
 
 
   while (counter < size_FB + 10){
-    //cout << "Next iter" << endl;
     temp = mpz_get_str(NULL, 10, T);
-    // cout << "The value of T is " <<  temp << endl;
 
     //initialize matrix which stores max power of a prime which divides any given polynomial evaluation
     power_storage = new int*[size_FB+1];
@@ -217,7 +214,6 @@ void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
     }
 
     // counting number of relation
-
     SI = generate_sieving_interval(N, size_SSI, T);
     mpz_set(T, T_hold);
     SI_SAVE = generate_sieving_interval(N, size_SSI, T);
@@ -231,36 +227,24 @@ void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
       mpz_init_set_ui(b, FB[i].b);
 
       //find smallest indices such that the polynomial evaluation at that index is divisble by p
-
-
       init1 = prime_find_min(size_SI, a, p, min1, T_hold);
       init2 = prime_find_min(size_SI, b, p, min2, T_hold);
       // string temp;
       temp = mpz_get_str(NULL, 10, T_hold);
-
-      // cout << "We find init1: "  << init1 << " and init2: " << init2 << "for the prime " << p << "and value " << temp << endl;
-
-      //prepare for for loop
       step = mpz_get_ui (p);
-      // mpz_t res;
-      // mpz_init(res);
 
       //go ahead and do all of the divisions
       prime_divide(SI, power_storage, &counter, size_SSI, size_FB, init1, step, i);
       prime_divide(SI, power_storage, &counter, size_SSI, size_FB, init2, step, i);
 
-      // cout << "count:" << counter << endl;
 
-
-      // //if there are enough relations, it is time to return
+      //if there are enough relations, it is time to return
       if (counter >= size_FB + 10){
-        // cout << "We are here!" << endl;
         break;
-        //return power_storage;
       }
     }
 
-    write_to_file(smooth_num_store, power_matrix_store, bit_matrix_store, 
+    write_to_file(smooth_num_store, power_matrix_store, bit_matrix_store,
                   size_SSI, size_FB, power_storage, SI_SAVE);
 
     delete SI;
@@ -290,21 +274,21 @@ void sieving_step(prime_element *FB, mpz_t N, int fbs, int pes){
 }
 
 /*
-Description: Helper function used in sieving_step. Here we divide 
-            elements in the sieving interval by primes when 
+Description: Helper function used in sieving_step. Here we divide
+            elements in the sieving interval by primes when
             applicable
-Params: (1) polynomial_element array pointer SI; 
+Params: (1) polynomial_element array pointer SI;
         (2) int double pointer to 2D-power_storage array;
         (3) int pointer counter (keeps track of number of relations)
         (4) int sieving interval size (size_SI)
         (5) int factorbase size (size_FB)
         (6) int smallest index for which this prime divides
             this sieving element
-        (7) int prime (prime of interest) 
+        (7) int prime (prime of interest)
         (8) int i (index of the prime of interest)
 Return [Explicit]: Nothing
-Return [Implicit]: [Note: We write in place using passed pointers]: 
-                    (1) polynomial_element array pointer SI; 
+Return [Implicit]: [Note: We write in place using passed pointers]:
+                    (1) polynomial_element array pointer SI;
                     (2) int double pointer to 2D-power_storage array;
                     (3) int pointer counter (keeping track of number of relations)
 */
@@ -347,14 +331,14 @@ Description: Finds the smallest index in the current sieving interval
               where the value stored is a multiple of our prime
               of interest, p.
 Params: (1) int size of interval (size_SI)
-        (2) mpz_t solution (a) to tonelli shanks (Q(x) is divisble 
+        (2) mpz_t solution (a) to tonelli shanks (Q(x) is divisble
             by p when evaluated at numbers of form a+kp and b+kp)
         (3) mpz_t prime of interest p
         (4) mpz_t min: the smallest index in the sieving intereval
             where the element is divisible by p
         (5) mpz_t T, where T is the input to our polynomial
             function Q(T) = T^2 - N
-        (6) mpz_t idx:  
+        (6) mpz_t idx:
 Return: unsigned int min: the smallest index in the sieving intereval
             where the element is divisible by p
 */
@@ -383,19 +367,19 @@ int prime_find_min(int size_SI, mpz_t a, mpz_t p, mpz_t min, mpz_t T){
 }
 
 /*
-Description: writing to file smooth numbers, power matrix, and bit matrix. 
+Description: writing to file smooth numbers, power matrix, and bit matrix.
 Params: (1) ofstream reference smooth_numstore
         (2) ofstream reference power_matrix_store
         (3) ofstream reference bit_matrix_store
-        (4) int size_SSI: sieving sub interval size (currently sieving 
+        (4) int size_SSI: sieving sub interval size (currently sieving
             interval)
         (5) int size_FB: size of factor base
         (6) int double pointer to 2d array of power_storage
         (7) pointer to polynomial_element array SI_SAVE
 Return: Nothing
-Output: [All Text Files]: (1) Power_Matrix.txt (matrix representation of
-                              prime powers of found relations); 
-                          (2) Bit_Matrix.txt (Power_Matrix mod 2); 
+Output [All Text Files]: (1) Power_Matrix.txt (matrix representation of
+                              prime powers of found relations);
+                          (2) Bit_Matrix.txt (Power_Matrix mod 2);
                           (3) Smooth_Numbers.txt (Numbers we have found to be smooth)
 */
 void write_to_file(ofstream& smooth_num_store, ofstream& power_matrix_store,
@@ -404,7 +388,7 @@ void write_to_file(ofstream& smooth_num_store, ofstream& power_matrix_store,
 
   string temp;
   int val;
-    
+
   for (int j = 0; j < size_SSI; j++){
     if (power_storage[size_FB][j] == 1){
       temp = mpz_get_str(NULL, 10, SI_SAVE[j].poly);
